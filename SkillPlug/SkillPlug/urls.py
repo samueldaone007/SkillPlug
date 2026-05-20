dbe80a8b0fd983e883916083b269e7dc358e7062
samueldaone007 <samuelafolayan017@gmqail.com>
2026-05-19 17:05:46 -0700

    Update commit

--- SkillPlug/SkillPlug/urls.py
+++ SkillPlug/SkillPlug/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from apps.marketplace.views import HomeView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', HomeView.as_view(), name='home'),
+    path('marketplace/', include('apps.marketplace.urls')),
+    path('jobs/', include('apps.jobs.urls')),
+    path('profiles/', include('apps.accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
